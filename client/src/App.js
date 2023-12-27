ed3110c054b8cae31f33f293520b80fd79b2a3e1
BharadwajSathya <bharadwajinamahanthi@gmail.com>
2023-12-27 23:18:52 +0530

    production is also working

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route exact path="/" element={<Home />} />
+        {/* <Route exact path="/" element={<Home />} /> */}
         <Route exact path="/home" element={<Home />} />
         <Route exact path="/about" element={<About />} />
         <Route exact path="/projects" element={<Projects />} />
